6c1806f19d5dd13fa7cd9b4e9003a96b51469b9e
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-05-01 17:17:11 +0000

    Initialize version

---
+++ java/nepxion-swing-demo/src/com/nepxion/demo/component/taskbar/DemoTaskBarPanel.java
@@ -0,0 +1,157 @@
+package com.nepxion.demo.component.taskbar;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JEditorPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import com.nepxion.swing.border.ComplexEtchedBorder;
+import com.nepxion.swing.border.ComplexSide;
+import com.nepxion.swing.border.ComplexTitleBorder;
+import com.nepxion.swing.handle.HandleManager;
+import com.nepxion.swing.icon.IconFactory;
+import com.nepxion.swing.lookandfeel.LookAndFeelTweak;
+import com.nepxion.swing.optionpane.JBasicOptionPane;
+import com.nepxion.swing.taskbar.JTaskAction;
+import com.nepxion.swing.taskbar.JTaskBar;
+import com.nepxion.swing.taskbar.JTaskGroup;
+
+public class DemoTaskBarPanel
+	extends JPanel
+{
+	public DemoTaskBarPanel()
+	{
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		
+		add(new TaskBarPanel());
+	}
+	
+	public class TaskBarPanel
+		extends JPanel
+	{
+		public TaskBarPanel()
+		{
+			setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+			setBorder(new ComplexTitleBorder(new ComplexEtchedBorder(ComplexEtchedBorder.LOWERED, ComplexSide.NORTH), "TaskBar"));
+			
+			final JTaskBar taskBar = new JTaskBar();
+			
+			// System TaskGroup
+			JTaskGroup systemTaskGroup = new JTaskGroup("File and Folder Tasks", IconFactory.getIcon("com/l2fprod/common/demo/icons/tasks-email.png"), "Everything you can execute on files and folders");
+			systemTaskGroup.setSpecial(true);
+			systemTaskGroup.add(new JTaskAction("Send by email", IconFactory.getIcon("com/l2fprod/common/demo/icons/tasks-email.png"), "Send by email")
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					JBasicOptionPane.showMessageDialog(HandleManager.getFrame(DemoTaskBarPanel.this), "Send by email", "Message", JBasicOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+			);
+			systemTaskGroup.add(new JTaskAction("Delete", IconFactory.getIcon("com/l2fprod/common/demo/icons/tasks-recycle.png"), "Delete")
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					JBasicOptionPane.showMessageDialog(HandleManager.getFrame(DemoTaskBarPanel.this), "Delete", "Message", JBasicOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+			);
+			taskBar.add(systemTaskGroup);
+			
+			// Office TaskGroup
+			JTaskGroup officeTaskGroup = new JTaskGroup("Office Tasks", IconFactory.getIcon("com/l2fprod/common/demo/icons/tasks-writedoc.png"), "Office Tasks");
+			officeTaskGroup.setExpanded(false);
+			officeTaskGroup.setScrollOnExpand(true);
+			officeTaskGroup.add(new JTaskAction("Write Document", IconFactory.getIcon("com/l2fprod/common/demo/icons/tasks-writedoc.png"), "Write Document")
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					JBasicOptionPane.showMessageDialog(HandleManager.getFrame(DemoTaskBarPanel.this), "Write Document", "Message", JBasicOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+			);
+			taskBar.add(officeTaskGroup);
+			
+			// SeeAlso TaskGroup
+			JTaskGroup seeAlsoTaskGroup = new JTaskGroup("See Also", "See Also");
+			seeAlsoTaskGroup.setCollapsable(false);
+			seeAlsoTaskGroup.add(new JTaskAction("The Internet", IconFactory.getIcon("com/l2fprod/common/demo/icons/tasks-internet.png"), "A cool web resource")
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					JBasicOptionPane.showMessageDialog(HandleManager.getFrame(DemoTaskBarPanel.this), "The Internet", "Message", JBasicOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+			);
+			seeAlsoTaskGroup.add(new JTaskAction("Help Center", IconFactory.getIcon("com/l2fprod/common/demo/icons/tasks-question.png"), "The place where you can't find anything")
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					JBasicOptionPane.showMessageDialog(HandleManager.getFrame(DemoTaskBarPanel.this), "Help Center", "Message", JBasicOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+			);
+			taskBar.add(seeAlsoTaskGroup);
+			
+			// Details TaskGroup
+			JTaskGroup detailsTaskGroup = new JTaskGroup("Details", "Details");
+			detailsTaskGroup.setAnimated(false);
+			JEditorPane detailsText = new JEditorPane("text/html", "<html>");
+			LookAndFeelTweak.makeMultilineLabel(detailsText);
+			LookAndFeelTweak.htmlize(detailsText);			
+			detailsText.setText("<html><b>My Computer</b><br>Very special folder</html>");
+			detailsTaskGroup.add(detailsText);   
+			taskBar.add(detailsTaskGroup);
+
+			// Management
+			JTaskGroup managementTaskGroup = new JTaskGroup("Management", "Management");
+			managementTaskGroup.add(new JTaskAction("Add TaskGroup", "Add TaskGroup")
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					JTaskGroup newTaskGroup = new JTaskGroup("New TaskGroup", "New TaskGroup");
+					newTaskGroup.add(new JTaskAction("New Task", "New Task")
+					{
+						public void actionPerformed(ActionEvent e)
+						{
+							JBasicOptionPane.showMessageDialog(HandleManager.getFrame(DemoTaskBarPanel.this), "New Task", "Message", JBasicOptionPane.INFORMATION_MESSAGE);
+						}
+					}
+					);
+					taskBar.add(newTaskGroup);
+					revalidate();
+					repaint();					
+				}
+			}
+			);
+			managementTaskGroup.add(new JTaskAction("Delete TaskGroup", "Delete TaskGroup")
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					taskBar.remove(0);
+					revalidate();
+					repaint();					
+				}
+			}
+			);			
+			taskBar.add(managementTaskGroup);
+            		
+			JScrollPane taskBarScrollPane = new JScrollPane();
+			taskBarScrollPane.getViewport().add(taskBar);				
+			add(taskBarScrollPane);			
+		}
+	}
+}
