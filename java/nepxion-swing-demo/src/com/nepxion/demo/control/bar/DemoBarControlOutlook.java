5dc3fc0ec076a05262f745a83ded8f332575cc3c
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-05-01 17:18:04 +0000

    Rectory

--- java/nepxion-swing-demo/src/com/nepxion/demo/control/bar/DemoBarControlOutlook.java
+++ java/nepxion-swing-demo/src/com/nepxion/demo/control/bar/DemoBarControlOutlook.java
@@ -1,6 +1,41 @@
 package com.nepxion.demo.control.bar;
 
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.awt.Dimension;
+
+import javax.swing.SwingConstants;
+
+import com.nepxion.demo.common.DemoToggleContentPanel;
+import com.nepxion.demo.common.DemoToggleOutlook;
+import com.nepxion.swing.button.ButtonManager;
+import com.nepxion.swing.panel.toggle.JToggleActionButton;
+
 public class DemoBarControlOutlook
+	extends DemoToggleOutlook
 {
-	
+	public DemoBarControlOutlook()
+	{
+		JToggleActionButton taskBarButton = new JToggleActionButton(DemoToggleContentPanel.getInstance(), new DemoTaskBarTogglePanel());
+        addButton(taskBarButton); 
+        
+		JToggleActionButton outlookBarButton = new JToggleActionButton(DemoToggleContentPanel.getInstance(), new DemoOutlookBarTogglePanel());
+        addButton(outlookBarButton); 
+        
+		JToggleActionButton buttonBarButton = new JToggleActionButton(DemoToggleContentPanel.getInstance(), new DemoButtonBarTogglePanel());
+        addButton(buttonBarButton);         
+        
+		JToggleActionButton statusBarButton = new JToggleActionButton(DemoToggleContentPanel.getInstance(), new DemoStatusBarTogglePanel());
+        addButton(statusBarButton); 
+                
+        ButtonManager.updateUI(this, new Dimension(50, 70), SwingConstants.VERTICAL);
+	}
 }
