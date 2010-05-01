925f95eba077f19a06c0bc085fafa6b1c2d25036
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-05-01 17:19:13 +0000

    Initialize version

---
+++ java/nepxion-swing-demo/src/com/nepxion/demo/control/bar/DemoStatusBarTogglePanel.java
@@ -0,0 +1,49 @@
+package com.nepxion.demo.control.bar;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import javax.swing.Icon;
+
+import com.nepxion.demo.common.DemoTogglePanel;
+import com.nepxion.demo.common.DemoToggleTemplate;
+import com.nepxion.demo.component.button.DemoButtonPanel;
+import com.nepxion.swing.icon.IconFactory;
+import com.nepxion.swing.tabbedpane.JEclipseTabbedPane;
+
+public class DemoStatusBarTogglePanel
+	extends DemoTogglePanel
+{
+	public DemoStatusBarTogglePanel()
+	{
+		JEclipseTabbedPane toggleTabbedPane = getToggleTabbedPane();
+		toggleTabbedPane.addTab("Button", IconFactory.getSwingIcon("component/tool_bar_16.png"), new DemoToggleTemplate(new DemoButtonPanel()));
+	}
+	
+	public String getToggleName()
+	{
+		return "StatusBar";
+	}
+	
+	public Icon getToggleIcon()
+	{
+		return IconFactory.getSwingIcon("component/tool_bar_32.png");
+	}
+	
+	public Icon getToggleBannerIcon()
+	{
+		return IconFactory.getSwingIcon("component/tool_bar_32.png");
+	}
+	
+	public String getToggleDescription()
+	{
+		return "Multi-style StatusBar";
+	}
+}
---
+++ java/nepxion-swing-demo/src/com/nepxion/demo/control/bar/DemoTaskBarTogglePanel.java
@@ -0,0 +1,67 @@
+package com.nepxion.demo.control.bar;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import javax.swing.Icon;
+
+import com.nepxion.demo.common.DemoTogglePanel;
+import com.nepxion.demo.common.DemoToggleTemplate;
+import com.nepxion.demo.component.taskbar.DemoTaskBarPanel;
+import com.nepxion.swing.icon.IconFactory;
+import com.nepxion.swing.lookandfeel.LookAndFeelManager;
+import com.nepxion.swing.tabbedpane.JEclipseTabbedPane;
+
+public class DemoTaskBarTogglePanel
+	extends DemoTogglePanel
+{
+	public DemoTaskBarTogglePanel()
+	{
+		JEclipseTabbedPane toggleTabbedPane = getToggleTabbedPane();
+		
+		LookAndFeelManager.addonMetalLookAndFeel();
+		toggleTabbedPane.addTab("Metal L&F", IconFactory.getSwingIcon("component/tool_bar_16.png"), new DemoToggleTemplate(new DemoTaskBarPanel()));
+		
+		LookAndFeelManager.addonWindowsLookAndFeel();
+		toggleTabbedPane.addTab("Windows L&F", IconFactory.getSwingIcon("component/tool_bar_16.png"), new DemoToggleTemplate(new DemoTaskBarPanel()));
+		
+		LookAndFeelManager.addonLunaLookAndFeel();
+		toggleTabbedPane.addTab("Luna L&F", IconFactory.getSwingIcon("component/tool_bar_16.png"), new DemoToggleTemplate(new DemoTaskBarPanel()));
+		
+		LookAndFeelManager.addonHomesteadLookAndFeel();
+		toggleTabbedPane.addTab("Homestead L&F", IconFactory.getSwingIcon("component/tool_bar_16.png"), new DemoToggleTemplate(new DemoTaskBarPanel()));
+		
+		LookAndFeelManager.addonMetallicLookAndFeel();
+		toggleTabbedPane.addTab("Metallic L&F", IconFactory.getSwingIcon("component/tool_bar_16.png"), new DemoToggleTemplate(new DemoTaskBarPanel()));
+		
+		LookAndFeelManager.addonGlossyLookAndFeel();
+		toggleTabbedPane.addTab("Glossy L&F", IconFactory.getSwingIcon("component/tool_bar_16.png"), new DemoToggleTemplate(new DemoTaskBarPanel()));
+	}
+	
+	public String getToggleName()
+	{
+		return "TaskBar";
+	}
+	
+	public Icon getToggleIcon()
+	{
+		return IconFactory.getSwingIcon("component/tool_bar_32.png");
+	}
+	
+	public Icon getToggleBannerIcon()
+	{
+		return IconFactory.getSwingIcon("component/tool_bar_32.png");
+	}
+	
+	public String getToggleDescription()
+	{
+		return "Multi-style TaskBar";
+	}
+}
