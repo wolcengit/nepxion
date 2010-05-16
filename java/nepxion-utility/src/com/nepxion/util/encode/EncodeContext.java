f86e350941cd3710defd5fbc278b7cf6c5397954
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-05-16 13:27:53 +0000

    Initialize version

---
+++ java/nepxion-utility/src/com/nepxion/util/encode/EncodeContext.java
@@ -0,0 +1,26 @@
+package com.nepxion.util.encode;
+
+/**
+ * <p>Title: Nepxion Utility</p>
+ * <p>Description: Nepxion Utility Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+public class EncodeContext
+{
+    private static String charset = "ISO-8859-1";
+    
+    public static String getCharset()
+    {
+    	return charset;
+    }
+    
+    public static void setCharset(String value)
+    {
+    	charset = value;
+    }    
+}
