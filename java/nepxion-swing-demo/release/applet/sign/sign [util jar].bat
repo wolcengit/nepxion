c03b37b35ffc9aba2e1ffaf6bfe4bfc89dc81afa
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-06-13 05:44:15 +0000

    Initialize version

---
+++ java/nepxion-swing-demo/release/applet/sign/sign [util jar].bat
@@ -0,0 +1,10 @@
+jarsigner -keystore Security.store ../lib/util/commons-logging-1.1.1.jar Security
+jarsigner -keystore Security.store ../lib/util/dom4j-1.6.1.jar Security
+jarsigner -keystore Security.store ../lib/util/http-client-4.0.1.jar Security
+jarsigner -keystore Security.store ../lib/util/http-core-4.0.1.jar Security
+jarsigner -keystore Security.store ../lib/util/json.jar Security
+jarsigner -keystore Security.store ../lib/util/log4j-1.2.16.jar Security
+jarsigner -keystore Security.store ../lib/util/poi-3.6-20091214.jar Security
+jarsigner -keystore Security.store ../lib/util/proxool-0.9.1.jar Security
+jarsigner -keystore Security.store ../lib/util/proxool-cglib.jar Security
+jarsigner -keystore Security.store ../lib/util/servlet-api.jar Security
