d485bca0054d5e71f42cc8cf802b80e729f2e5c0
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-07-04 15:25:46 +0000

    Initialize version

---
+++ java/nepxion-swing-demo/src/com/nepxion/demo/component/gis/google/DemoGisPanel.java
@@ -0,0 +1,45 @@
+package com.nepxion.demo.component.gis.google;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+import com.nepxion.swing.border.ComplexEtchedBorder;
+import com.nepxion.swing.border.ComplexSide;
+import com.nepxion.swing.border.ComplexTitleBorder;
+import com.nepxion.swing.gis.google.JGisPanel;
+
+public class DemoGisPanel
+	extends JPanel
+{
+	public DemoGisPanel()
+	{
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		
+		add(new GisPanel());
+	}
+	
+	public class GisPanel
+		extends JPanel
+	{
+		public GisPanel()
+		{
+			setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+			setBorder(new ComplexTitleBorder(new ComplexEtchedBorder(ComplexEtchedBorder.LOWERED, ComplexSide.NORTH), "GIS"));
+			
+			JGisPanel gisPanel = new JGisPanel();
+			add(gisPanel);
+		}
+	}
+}
