e2b848249b7e9ef09572d9e8af0bd19f6065115f
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-07-05 08:57:53 +0000

    Initialize version

---
+++ java/nepxion-utility/src/com/nepxion/util/gis/google/geo/GeoConstants.java
@@ -0,0 +1,16 @@
+package com.nepxion.util.gis.google.geo;
+
+/**
+ * <p>Title: Nepxion Utility</p>
+ * <p>Description: Nepxion Utility Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+public class GeoConstants
+{
+	
+}
