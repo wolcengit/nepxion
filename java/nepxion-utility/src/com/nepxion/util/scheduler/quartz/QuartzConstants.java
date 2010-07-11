e3711fc7acd76ffc059e7479595a7fdd84f8ceb2
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-07-11 17:16:05 +0000

    Initialize version

---
+++ java/nepxion-utility/src/com/nepxion/util/scheduler/quartz/QuartzConstants.java
@@ -0,0 +1,16 @@
+package com.nepxion.util.scheduler.quartz;
+
+/**
+ * <p>Title: Nepxion Utility</p>
+ * <p>Description: Nepxion Utility Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+public interface QuartzConstants
+{
+	public static final String CONFIG_FILE_PATH = "quartz.properties";
+}
