599f2260ed0ba98aadfa05602f645f3651870342
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-07-12 13:40:31 +0000

    Initialize version

---
+++ java/nepxion-utility/src/com/nepxion/util/scheduler/quartz/QuartzContext.java
@@ -0,0 +1,99 @@
+package com.nepxion.util.scheduler.quartz;
+
+/**
+ * <p>Title: Nepxion Utility</p>
+ * <p>Description: Nepxion Utility Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.net.URL;
+import java.util.Properties;
+
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+
+import com.nepxion.util.database.pool.proxool.ProxoolConnectionPool;
+import com.nepxion.util.log.Logger;
+
+public class QuartzContext
+	implements QuartzConstants
+{
+	private static QuartzSchedulerFactory factory;
+	private static Scheduler scheduler;
+	
+	public static void register()
+		throws SchedulerException
+	{
+		register(CONFIG_FILE_PATH);
+	}
+	
+	public static void register(String filePath)
+		throws SchedulerException
+	{
+		Logger.info(QuartzContext.class, "--- Quartz Scheduler Initialization Start ---");
+		
+		try
+		{
+			factory = new QuartzSchedulerFactory(filePath);
+			scheduler = factory.getScheduler();
+		}
+		catch (SchedulerException e)
+		{
+			Logger.fatal(QuartzContext.class, e);
+			throw e;
+		}
+		
+		Logger.info(QuartzContext.class, "---- Quartz Scheduler Initialization End ----");
+	}
+	
+	public static void register(Properties properties)
+		throws SchedulerException
+	{
+		Logger.info(QuartzContext.class, "--- Quartz Scheduler Initialization Start ---");
+		
+		try
+		{
+			factory = new QuartzSchedulerFactory(properties);
+			scheduler = factory.getScheduler();
+		}
+		catch (SchedulerException e)
+		{
+			Logger.fatal(QuartzContext.class, e);
+			throw e;
+		}
+		
+		Logger.info(QuartzContext.class, "---- Quartz Scheduler Initialization End ----");
+	}
+	
+	public static void register(URL codeBaseURL)
+		throws Exception
+	{
+		Properties properties = new Properties();
+		try
+		{
+			codeBaseURL = new URL(codeBaseURL + CONFIG_FILE_PATH);
+			properties.load(codeBaseURL.openStream());
+			
+			register(properties);
+		}
+		catch (Exception e)
+		{
+			Logger.fatal(ProxoolConnectionPool.class, e);
+			throw e;
+		}
+	}
+	
+	public static QuartzSchedulerFactory getFactory()
+	{
+		return factory;
+	}
+	
+	public static Scheduler getScheduler()
+	{
+		return scheduler;
+	}
+}
