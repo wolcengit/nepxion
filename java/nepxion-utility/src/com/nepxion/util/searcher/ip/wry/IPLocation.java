ef42825d1a745ea6bdf7940f468b821f125ef19a
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-07-14 09:21:37 +0000

    Initialize version

---
+++ java/nepxion-utility/src/com/nepxion/util/searcher/ip/wry/IPLocation.java
@@ -0,0 +1,56 @@
+package com.nepxion.util.searcher.ip.wry;
+
+/**
+ * <p>Title: Nepxion Utility</p>
+ * <p>Description: Nepxion Utility Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.io.Serializable;
+
+public class IPLocation
+	implements Serializable
+{
+	private static final long serialVersionUID = 5429316904963543813L;
+
+	private String country;
+	private String region;
+	
+	public IPLocation()
+	{
+		country = "";
+		region = "";
+	}
+	
+	public String getCountry()
+	{
+		return country.trim().equals("CZ88.NET") ?  "" : country.trim();
+	}
+	
+	public void setCountry(String country)
+	{
+		this.country = country;
+	}
+	
+	public String getRegion()
+	{
+		return region.trim().equals("CZ88.NET") ?  "" : region.trim();
+	}
+	
+	public void setRegion(String region)
+	{
+		this.region = region;
+	}
+	
+	public IPLocation clone()
+	{
+		IPLocation ipLocation = new IPLocation();
+		ipLocation.country = country;
+		ipLocation.region = region;
+		return ipLocation;
+	}
+}
