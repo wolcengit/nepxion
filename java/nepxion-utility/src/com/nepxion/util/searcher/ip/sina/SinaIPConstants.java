db5cbea49ffe42256d2fc9c92bfb0e948d225072
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-07-15 13:19:16 +0000

    Initialize version

---
+++ java/nepxion-utility/src/com/nepxion/util/searcher/ip/sina/SinaIPConstants.java
@@ -0,0 +1,16 @@
+package com.nepxion.util.searcher.ip.sina;
+
+/**
+ * <p>Title: Nepxion Utility</p>
+ * <p>Description: Nepxion Utility Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+public interface SinaIPConstants
+{
+	public static final String URL = "http://counter.sina.com.cn/ip";
+}
---
+++ java/nepxion-utility/src/com/nepxion/util/searcher/ip/sina/SinaIPSearcher.java
@@ -0,0 +1,103 @@
+package com.nepxion.util.searcher.ip.sina;
+
+/**
+ * <p>Title: Nepxion Utility</p>
+ * <p>Description: Nepxion Utility Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.net.URI;
+import java.util.StringTokenizer;
+
+import com.nepxion.util.net.http.apache.ClientInvoker;
+import com.nepxion.util.net.http.apache.ClientParameterList;
+import com.nepxion.util.net.http.apache.ClientRequestGet;
+
+public class SinaIPSearcher
+	implements SinaIPConstants
+{
+	private String charset = "GBK";
+	private ClientInvoker clientInvoker;
+	
+	public SinaIPSearcher()
+	{
+		this("GBK");
+	}
+	
+	public SinaIPSearcher(String charset)
+	{
+		this.charset = charset;
+		
+		this.clientInvoker = new ClientInvoker();
+	}
+	
+	public SinaIPEntity search(String ip)
+		throws Exception
+	{
+		String responseText = getResponseText(ip);
+		return createIPEntity(responseText);
+	}
+	
+	public SinaIPEntity search()
+		throws Exception
+	{
+		String responseText = getResponseText(null);
+		return createIPEntity(responseText);
+	}
+	
+	private SinaIPEntity createIPEntity(String text)
+	{
+		if (text == null || text.equals(""))
+		{
+			return null;
+		}
+		
+		SinaIPEntity ipEntity = new SinaIPEntity();
+		
+		StringTokenizer stringTokenizer = new StringTokenizer(text, ",");
+		
+		String ip = stringTokenizer.nextToken();
+		ip = ip.substring(ip.indexOf("\"") + 1, ip.lastIndexOf("\""));
+		ipEntity.setIP(ip);
+		
+		String country = stringTokenizer.nextToken();
+		country = country.substring(country.indexOf("\"") + 1, country.lastIndexOf("\""));
+		ipEntity.setCountry(country);
+		
+		String province = stringTokenizer.nextToken();
+		province = province.substring(province.indexOf("\"") + 1, province.lastIndexOf("\""));
+		ipEntity.setProvince(province);
+		
+		String city = stringTokenizer.nextToken();
+		city = city.substring(city.indexOf("\"") + 1, city.lastIndexOf("\""));
+		ipEntity.setCity(city);
+		
+		String corp = stringTokenizer.nextToken();
+		corp = corp.substring(corp.indexOf("\"") + 1);
+		corp = corp.substring(0, corp.indexOf("\""));
+		ipEntity.setCorp(corp);
+		
+		return ipEntity;
+	}
+	
+	private String getResponseText(String ip)
+		throws Exception
+	{
+		ClientRequestGet clientRequestGet = new ClientRequestGet();
+		clientRequestGet.setURI(URI.create(URL));
+		
+		if (ip != null)
+		{
+			ClientParameterList clientParameterList = new ClientParameterList();
+			clientParameterList.put("ip", ip);
+			clientRequestGet.setParameterEntity(clientParameterList, charset);
+		}
+		
+		String responseText = clientInvoker.getResponseText(clientRequestGet, charset);
+		return responseText;
+	}
+}
