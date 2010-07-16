0352a18354afb51e876e1e7ccb3715546b8fbaff
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-07-16 14:07:55 +0000

    Initialize version

---
+++ java/nepxion-swing-demo/src/com/nepxion/demo/component/searcher/mobile/netease/DemoNeteaseMobilePanel.java
@@ -0,0 +1,45 @@
+package com.nepxion.demo.component.searcher.mobile.netease;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+import com.nepxion.swing.border.ComplexEtchedBorder;
+import com.nepxion.swing.border.ComplexSide;
+import com.nepxion.swing.border.ComplexTitleBorder;
+import com.nepxion.swing.searcher.mobile.netease.JNeteaseMobilePanel;
+
+public class DemoNeteaseMobilePanel
+	extends JPanel
+{
+	public DemoNeteaseMobilePanel()
+	{
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		
+		add(new NeteaseMobilePanel());
+	}
+	
+	public class NeteaseMobilePanel
+		extends JPanel
+	{
+		public NeteaseMobilePanel()
+		{
+			setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+			setBorder(new ComplexTitleBorder(new ComplexEtchedBorder(ComplexEtchedBorder.LOWERED, ComplexSide.NORTH), "手机归属地查询"));
+			
+			JNeteaseMobilePanel mobilePanel = new JNeteaseMobilePanel();			
+			add(mobilePanel);
+		}
+	}
+}
