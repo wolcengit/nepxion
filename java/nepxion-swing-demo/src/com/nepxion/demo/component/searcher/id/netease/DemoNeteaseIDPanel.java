3b47b2186ea13b39efba48d33bb89424c05bfc12
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-07-16 08:09:25 +0000

    Initialize version

---
+++ java/nepxion-swing-demo/src/com/nepxion/demo/component/searcher/id/netease/DemoNeteaseIDPanel.java
@@ -0,0 +1,45 @@
+package com.nepxion.demo.component.searcher.id.netease;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+import com.nepxion.swing.border.ComplexEtchedBorder;
+import com.nepxion.swing.border.ComplexSide;
+import com.nepxion.swing.border.ComplexTitleBorder;
+import com.nepxion.swing.searcher.id.netease.JNeteaseIDPanel;
+
+public class DemoNeteaseIDPanel
+	extends JPanel
+{
+	public DemoNeteaseIDPanel()
+	{
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		
+		add(new IDPanel());
+	}
+	
+	public class IDPanel
+		extends JPanel
+	{
+		public IDPanel()
+		{
+			setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+			setBorder(new ComplexTitleBorder(new ComplexEtchedBorder(ComplexEtchedBorder.LOWERED, ComplexSide.NORTH), "身份证归属地查询"));
+			
+			JNeteaseIDPanel idPanel = new JNeteaseIDPanel();			
+			add(idPanel);
+		}
+	}
+}
