abb9b5e1f417629988c7dd32baa5562d086ceefc
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-07-16 13:20:54 +0000

    Initialize version

---
+++ java/nepxion-swing-demo/src/com/nepxion/demo/component/searcher/mobile/DemoLocatingMobilePanel.java
@@ -0,0 +1,45 @@
+package com.nepxion.demo.component.searcher.mobile;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+import com.nepxion.swing.border.ComplexEtchedBorder;
+import com.nepxion.swing.border.ComplexSide;
+import com.nepxion.swing.border.ComplexTitleBorder;
+import com.nepxion.swing.searcher.mobile.locating.JLocatingMobilePanel;
+
+public class DemoLocatingMobilePanel
+	extends JPanel
+{
+	public DemoLocatingMobilePanel()
+	{
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		
+		add(new CellPanel());
+	}
+	
+	public class CellPanel
+		extends JPanel
+	{
+		public CellPanel()
+		{
+			setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+			setBorder(new ComplexTitleBorder(new ComplexEtchedBorder(ComplexEtchedBorder.LOWERED, ComplexSide.NORTH), "根据移动信息查询地理位置"));
+			
+			JLocatingMobilePanel mobilePanel = new JLocatingMobilePanel();			
+			add(mobilePanel);
+		}
+	}
+}
