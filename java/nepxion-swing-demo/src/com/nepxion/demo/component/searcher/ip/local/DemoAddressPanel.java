570362aa45b2f0dba4c4962c362af3ab4bfee156
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-07-17 20:22:31 +0000

    Initialize version

---
+++ java/nepxion-swing-demo/src/com/nepxion/demo/component/searcher/ip/local/DemoAddressPanel.java
@@ -0,0 +1,45 @@
+package com.nepxion.demo.component.searcher.ip.local;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+import com.nepxion.swing.border.ComplexEtchedBorder;
+import com.nepxion.swing.border.ComplexSide;
+import com.nepxion.swing.border.ComplexTitleBorder;
+import com.nepxion.swing.searcher.ip.local.JAddressPanel;
+
+public class DemoAddressPanel
+	extends JPanel
+{
+	public DemoAddressPanel()
+	{
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		
+		add(new AddressPanel());
+	}
+	
+	public class AddressPanel
+		extends JPanel
+	{
+		public AddressPanel()
+		{
+			setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+			setBorder(new ComplexTitleBorder(new ComplexEtchedBorder(ComplexEtchedBorder.LOWERED, ComplexSide.NORTH), "模糊查询地理位置"));
+			
+			JAddressPanel addressPanel = new JAddressPanel();			
+			add(addressPanel);
+		}
+	}
+}
