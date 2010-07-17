8b6456f22452da3ecb970bd1b2c762e242d7218a
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-07-17 04:29:09 +0000

    Initialize version

---
+++ java/nepxion-swing-demo/src/com/nepxion/demo/component/searcher/ip/sina/DemoSinaIPPanel.java
@@ -0,0 +1,45 @@
+package com.nepxion.demo.component.searcher.ip.sina;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+import com.nepxion.swing.border.ComplexEtchedBorder;
+import com.nepxion.swing.border.ComplexSide;
+import com.nepxion.swing.border.ComplexTitleBorder;
+import com.nepxion.swing.searcher.ip.sina.JSinaIPPanel;
+
+public class DemoSinaIPPanel
+	extends JPanel
+{
+	public DemoSinaIPPanel()
+	{
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		
+		add(new SinaIPPanel());
+	}
+	
+	public class SinaIPPanel
+		extends JPanel
+	{
+		public SinaIPPanel()
+		{
+			setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+			setBorder(new ComplexTitleBorder(new ComplexEtchedBorder(ComplexEtchedBorder.LOWERED, ComplexSide.NORTH), "根据IP地址搜索地理位置"));
+			
+			JSinaIPPanel ipPanel = new JSinaIPPanel();
+			add(ipPanel);
+		}
+	}
+}
---
+++ java/nepxion-swing-demo/src/com/nepxion/demo/component/searcher/ip/tencent/DemoTencentIPPanel.java
@@ -0,0 +1,45 @@
+package com.nepxion.demo.component.searcher.ip.tencent;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+import com.nepxion.swing.border.ComplexEtchedBorder;
+import com.nepxion.swing.border.ComplexSide;
+import com.nepxion.swing.border.ComplexTitleBorder;
+import com.nepxion.swing.searcher.ip.tencent.JTencentIPPanel;
+
+public class DemoTencentIPPanel
+	extends JPanel
+{
+	public DemoTencentIPPanel()
+	{
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		
+		add(new TencentIPPanel());
+	}
+	
+	public class TencentIPPanel
+		extends JPanel
+	{
+		public TencentIPPanel()
+		{
+			setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+			setBorder(new ComplexTitleBorder(new ComplexEtchedBorder(ComplexEtchedBorder.LOWERED, ComplexSide.NORTH), "根据IP地址搜索地理位置"));
+			
+			JTencentIPPanel ipPanel = new JTencentIPPanel();
+			add(ipPanel);
+		}
+	}
+}
