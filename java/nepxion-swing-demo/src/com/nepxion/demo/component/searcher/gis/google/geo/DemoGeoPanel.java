e3a0930ad42e0268145d50869d5b0b46516bb76b
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-07-17 19:26:26 +0000

    Initialize version

---
+++ java/nepxion-swing-demo/src/com/nepxion/demo/component/searcher/gis/google/geo/DemoGeoPanel.java
@@ -0,0 +1,45 @@
+package com.nepxion.demo.component.searcher.gis.google.geo;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+import com.nepxion.swing.border.ComplexEtchedBorder;
+import com.nepxion.swing.border.ComplexSide;
+import com.nepxion.swing.border.ComplexTitleBorder;
+import com.nepxion.swing.searcher.gis.google.geo.JGeoPanel;
+
+public class DemoGeoPanel
+	extends JPanel
+{
+	public DemoGeoPanel()
+	{
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		
+		add(new GeoPanel());
+	}
+	
+	public class GeoPanel
+		extends JPanel
+	{
+		public GeoPanel()
+		{
+			setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+			setBorder(new ComplexTitleBorder(new ComplexEtchedBorder(ComplexEtchedBorder.LOWERED, ComplexSide.NORTH), "模糊查询地理位置"));
+			
+			JGeoPanel geoPanel = new JGeoPanel();			
+			add(geoPanel);
+		}
+	}
+}
