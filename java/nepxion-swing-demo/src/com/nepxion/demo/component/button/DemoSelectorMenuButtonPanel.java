adee14950ebe9a438ceba811a787a521c0f756d6
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-07-19 02:32:45 +0000

    Refactory

--- java/nepxion-swing-demo/src/com/nepxion/demo/component/button/DemoSelectorMenuButtonPanel.java
+++ java/nepxion-swing-demo/src/com/nepxion/demo/component/button/DemoSelectorMenuButtonPanel.java
@@ -15,14 +15,21 @@ import java.awt.Dimension;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.tree.TreeNode;
 
 import com.nepxion.swing.border.ComplexEtchedBorder;
 import com.nepxion.swing.border.ComplexSide;
 import com.nepxion.swing.border.ComplexTitleBorder;
 import com.nepxion.swing.button.ButtonManager;
+import com.nepxion.swing.button.JBasicSelectorMenuButton;
+import com.nepxion.swing.handle.HandleManager;
+import com.nepxion.swing.icon.IconFactory;
 import com.nepxion.swing.layout.filed.FiledLayout;
-import com.nepxion.swing.searcher.zone.local.JZoneSelectorButton;
-import com.nepxion.swing.textfield.number.JNumberTextField;
+import com.nepxion.swing.optionpane.JBasicOptionPane;
+import com.nepxion.swing.renderer.tree.TreeElementCellRenderer;
+import com.nepxion.swing.textfield.JBasicTextField;
+import com.nepxion.swing.tree.JBasicTree;
 
 public class DemoSelectorMenuButtonPanel
 	extends JPanel
@@ -41,10 +48,10 @@ public class DemoSelectorMenuButtonPanel
 		public SelectorMenuButtonPanel()
 		{
 			setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-			setBorder(new ComplexTitleBorder(new ComplexEtchedBorder(ComplexEtchedBorder.LOWERED, ComplexSide.NORTH), "Selector Menu Button"));
+			setBorder(new ComplexTitleBorder(new ComplexEtchedBorder(ComplexEtchedBorder.LOWERED, ComplexSide.NORTH), "Flat ButtonUI"));
 			
-			JNumberTextField textField = new JNumberTextField(12, 0);
-			JZoneSelectorButton selectorButton = new JZoneSelectorButton(textField);
+			JBasicTextField textField = new JBasicTextField();
+			SelectorMenuButton selectorButton = new SelectorMenuButton(textField);
 			
 			JPanel panel = new JPanel();
 			panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
@@ -52,6 +59,59 @@ public class DemoSelectorMenuButtonPanel
 			panel.add(selectorButton);
 			ButtonManager.updateUI(panel, new Dimension(26, 22));			
 			add(panel);
+		}
+	}
+	
+	public class SelectorMenuButton
+		extends JBasicSelectorMenuButton
+	{
+		private JBasicTextField textField;
+		private JBasicTree tree;
+		
+		public SelectorMenuButton(JBasicTextField textField)
+		{
+			super(IconFactory.getSwingIcon("property.png"), "Selector MenuButton");
+			
+			this.textField = textField;
+			
+			tree = new JBasicTree()
+			{
+				public void executeDoubleClicked(TreeNode treeNode)
+				{
+					if (treeNode.isLeaf())
+					{
+						doConfirm();
+					}
+				}
+			};
+					
+			tree.setCellRenderer(new TreeElementCellRenderer(TreeElementCellRenderer.NIMBUS_STYLE, 20));
+			tree.setSelectionMode(JBasicTree.SINGLE_TREE_SELECTION);			
+			tree.expandAll();
+			
+			JScrollPane scrollPane = new JScrollPane(tree);
+			scrollPane.setPreferredSize(new Dimension(300, 300));
+			
+			setContentPane(scrollPane);
+		}
+		
+		public boolean confirm()
+		{
+			TreeNode treeNode = tree.getSelectionTreeNode();
+			if (treeNode == null || !treeNode.isLeaf())
+			{
+				JBasicOptionPane.showMessageDialog(HandleManager.getFrame(this), "Please select child tree node", "Message", JBasicOptionPane.WARNING_MESSAGE);
+				return false;
+			}
+			
+			textField.setText(treeNode.toString());
+			
+			return true;
+		}
+		
+		public boolean cancel()
+		{
+			return true;
 		}
 	}
 }
