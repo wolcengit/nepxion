638781a34cf29ea2778fd6885ebd29dafa601413
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-07-23 03:20:28 +0000

    Initialize version

---
+++ java/nepxion-swing-demo/src/com/nepxion/demo/component/slide/DemoRangeSliderPanel.java
@@ -0,0 +1,47 @@
+package com.nepxion.demo.component.slide;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+import com.nepxion.swing.border.ComplexEtchedBorder;
+import com.nepxion.swing.border.ComplexSide;
+import com.nepxion.swing.border.ComplexTitleBorder;
+import com.nepxion.swing.layout.filed.FiledLayout;
+import com.nepxion.swing.spinner.JPointSpinnerField;
+
+public class DemoRangeSliderPanel
+	extends JPanel
+{
+	public DemoRangeSliderPanel()
+	{
+		setLayout(new FiledLayout(FiledLayout.COLUMN, FiledLayout.FULL, 0));
+		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		
+		add(new RangeSliderPanel());
+	}
+	
+	public class RangeSliderPanel
+		extends JPanel
+	{
+		public RangeSliderPanel()
+		{
+			setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+			setBorder(new ComplexTitleBorder(new ComplexEtchedBorder(ComplexEtchedBorder.LOWERED, ComplexSide.NORTH), "Point Spinner"));
+			
+
+		}
+	}
+}
