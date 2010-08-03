3fb9255d3ffc3bcab5ad89e5daa78016468ef7ad
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-08-03 05:01:30 +0000

    Refactory

--- java/nepxion-swing-demo/release/applet/sign/sign [swing all jar].bat
+++ java/nepxion-swing-demo/release/applet/sign/sign [swing all jar].bat
@@ -5,7 +5,7 @@ jarsigner -keystore Security.store ../lib/swing/itext-asian.jar Security
 jarsigner -keystore Security.store ../lib/swing/jacob.jar Security
 jarsigner -keystore Security.store ../lib/swing/jcommon-1.0.16.jar Security
 jarsigner -keystore Security.store ../lib/swing/jdesktop-swing-worker-1.2.jar Security
-jarsigner -keystore Security.store ../lib/swing/jdesktop-swingx-1.6.jar Security
+jarsigner -keystore Security.store ../lib/swing/jdesktop-swingx-1.6.1.jar Security
 jarsigner -keystore Security.store ../lib/swing/jfreechart-1.0.13.jar Security
 jarsigner -keystore Security.store ../lib/swing/jfreechart-1.0.13-experimental.jar Security
 jarsigner -keystore Security.store ../lib/swing/jgoodies-forms-1.3.0.jar Security
