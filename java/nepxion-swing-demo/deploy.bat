3fb6c7a61d4d255c169ffb6b438210a8cdeab107
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-08-17 02:12:44 +0000

    Initialize version

---
+++ java/nepxion-swing-demo/deploy.bat
@@ -0,0 +1,19 @@
+@echo on
+@echo =============================================================
+@echo $                                                           $ 
+@echo $                     Nepxion Deployment                    $  
+@echo $                                                           $ 
+@echo $                                                           $
+@echo $                                                           $
+@echo $  Nepxion Technologies All Right Reserved                  $
+@echo $  Copyright(C) 2010                                        $
+@echo $                                                           $
+@echo =============================================================
+@echo.
+
+copy release\application\lib\nepxion-util.jar release\applet\lib
+copy release\application\lib\nepxion-swing.jar release\applet\lib
+copy release\application\lib\nepxion-swing-demo.jar release\applet\lib
+
+cd release\applet\sign\
+sign-[nepxion-all].bat
