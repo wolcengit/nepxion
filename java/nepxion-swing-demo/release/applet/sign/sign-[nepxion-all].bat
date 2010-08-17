89ef1f4cd60ae48b07e238209b934ccc5a55015b
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-08-17 02:09:05 +0000

    Initialize version

---
+++ java/nepxion-swing-demo/release/applet/sign/sign-[nepxion-all].bat
@@ -0,0 +1,3 @@
+jarsigner -keystore Security.store -storepass 888888 ../lib/nepxion-util.jar Security
+jarsigner -keystore Security.store -storepass 888888 ../lib/nepxion-swing.jar Security
+jarsigner -keystore Security.store -storepass 888888 ../lib/nepxion-swing-demo.jar Security
---
+++ java/nepxion-swing-demo/release/applet/sign/sign-[util-jar].bat
@@ -0,0 +1,13 @@
+jarsigner -keystore Security.store ../lib/util/commons-logging-1.1.1.jar Security
+jarsigner -keystore Security.store ../lib/util/dom4j-1.6.1.jar Security
+jarsigner -keystore Security.store ../lib/util/http-client-4.0.1.jar Security
+jarsigner -keystore Security.store ../lib/util/http-core-4.0.1.jar Security
+jarsigner -keystore Security.store ../lib/util/json.jar Security
+jarsigner -keystore Security.store ../lib/util/log4j-1.2.16.jar Security
+jarsigner -keystore Security.store ../lib/util/poi-3.6-20091214.jar Security
+jarsigner -keystore Security.store ../lib/util/proxool-0.9.1.jar Security
+jarsigner -keystore Security.store ../lib/util/proxool-cglib.jar Security
+jarsigner -keystore Security.store ../lib/util/quartz-all-1.8.3.jar Security
+jarsigner -keystore Security.store ../lib/util/servlet-api.jar Security
+jarsigner -keystore Security.store ../lib/util/slf4j-api-1.6.1.jar Security
+jarsigner -keystore Security.store ../lib/util/slf4j-log4j12-1.6.1.jar Security
