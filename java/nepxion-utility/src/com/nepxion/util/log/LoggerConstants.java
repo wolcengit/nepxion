8b4fc6aa2d015621fd2de3e0322c83a14ff92526
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-08-23 17:23:41 +0000

    Initialize version

---
+++ java/nepxion-utility/src/com/nepxion/util/log/LoggerConstants.java
@@ -0,0 +1,16 @@
+package com.nepxion.util.log;
+
+/**
+ * <p>Title: Nepxion Utility</p>
+ * <p>Description: Nepxion Utility Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+public interface LoggerConstants
+{
+	public static final String CONFIG_FILE_PATH = "conf/log4j.properties";
+}
---
+++ java/nepxion-utility/src/com/nepxion/util/log/LoggerContext.java
@@ -0,0 +1,72 @@
+package com.nepxion.util.log;
+
+/**
+ * <p>Title: Nepxion Utility</p>
+ * <p>Description: Nepxion Utility Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.net.URL;
+import java.util.Properties;
+
+import org.apache.log4j.PropertyConfigurator;
+
+public class LoggerContext
+	implements LoggerConstants
+{
+	public static void register()
+		throws Exception
+	{
+		register(CONFIG_FILE_PATH);
+	}
+	
+	public static void register(String filePath)
+		throws Exception
+	{		
+		try
+		{
+			PropertyConfigurator.configure(filePath);
+		}
+		catch (Exception e)
+		{
+			Logger.fatal(LoggerContext.class, e);
+			throw e;
+		}
+	}
+	
+	public static void register(Properties properties)
+		throws Exception
+	{
+		try
+		{
+			PropertyConfigurator.configure(properties);
+		}
+		catch (Exception e)
+		{
+			Logger.fatal(LoggerContext.class, e);
+			throw e;
+		}
+	}
+	
+	public static void register(URL codeBase)
+		throws Exception
+	{
+		Properties properties = new Properties();
+		try
+		{
+			URL url = new URL(codeBase + CONFIG_FILE_PATH);
+			properties.load(url.openStream());
+
+			register(properties);
+		}
+		catch (Exception e)
+		{
+			Logger.fatal(LoggerContext.class, e);
+			throw e;
+		}
+	}
+}
