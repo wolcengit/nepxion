e156d27de0b87a671f4c775db55457dea1c8322b
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-11-30 08:04:30 +0000

    Initialize version

---
+++ java/nepxion-swing/src/com/nepxion/swing/preference/JPreference.java
@@ -0,0 +1,22 @@
+package com.nepxion.swing.preference;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import javax.swing.JPanel;
+
+import com.nepxion.swing.optionpane.IValidation;
+import com.nepxion.swing.toggle.ITogglePanel;
+
+public abstract class JPreference
+	extends JPanel implements ITogglePanel, IValidation
+{
+	
+}
