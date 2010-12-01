a891657cf1364b94e9588654318c42fdbe5ca976
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-12-01 02:16:38 +0000

    Initialize version

---
+++ java/nepxion-swing/src/com/nepxion/swing/table/AbstractTableAdapter.java
@@ -0,0 +1,74 @@
+package com.nepxion.swing.table;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+public abstract class AbstractTableAdapter
+	implements ITableAdapter	        
+{
+	public AbstractTableAdapter()
+	{
+		
+	}
+	
+	public boolean addRowPermitted()
+	{
+		return true;
+	}
+	
+	public Object addRow()
+	{
+		return null;
+	}
+	
+	
+	public boolean modifyRowPermitted(int selectedRow)
+	{
+		return true;
+	}
+	
+	public Object modifyRow(int selectedRow)
+	{
+		return null;
+	}
+	
+	
+	public boolean deleteRowPermitted(int selectedRow)
+	{
+		return true;
+	}
+	
+	public boolean deleteRow(int selectedRow)
+	{
+		return true;
+	}
+	
+	
+	public boolean deleteRowsPermitted(int[] selectedRows)
+	{
+		return true;
+	}
+	
+	public boolean deleteRows(int[] selectedRows)
+	{
+		return true;
+	}
+	
+	
+	public boolean clearPermitted()
+	{
+		return true;
+	}
+	
+	public boolean clear()
+	{
+		return true;
+	}
+}
