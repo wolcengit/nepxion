02c7d81793d8104e09ccb6d1694bcf89f6c7981f
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-12-20 05:54:44 +0000

    Initialize version

---
+++ java/nepxion-swing/src/com/nepxion/swing/list/IListAdapter.java
@@ -0,0 +1,45 @@
+package com.nepxion.swing.list;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.util.List;
+
+public interface IListAdapter
+{
+	public boolean addRowPermitted();
+	
+	public Object addRow();
+	
+	
+	public boolean modifyRowPermitted(int selectedRow);
+	
+	public Object modifyRow(int selectedRow);
+	
+	
+	public boolean deleteRowPermitted(int selectedRow);
+	
+	public boolean deleteRow(int selectedRow);
+	
+	
+	public boolean deleteRowsPermitted(int[] selectedRows);
+	
+	public boolean deleteRows(int[] selectedRows);
+	
+	
+	public boolean refreshPermitted();
+	
+	public List refresh();
+	
+	
+	public boolean clearPermitted();
+	
+	public boolean clear();
+}
