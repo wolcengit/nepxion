172207c6ea06be1011c169441dc8fa9930552619
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-12-23 06:22:55 +0000

    Refactory

--- java/nepxion-utility/src/com/nepxion/util/net/http/apache/ServerLogger.java
+++ java/nepxion-utility/src/com/nepxion/util/net/http/apache/ServerLogger.java
@@ -10,6 +10,8 @@ package com.nepxion.util.net.http.apache;
  * @version 1.0
  */
 
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -23,7 +25,7 @@ public class ServerLogger
 		log.info("Request Method     : " + method);
 		log.info("Request Charset    : " + charset);
 		log.info("Request Data Type  : " + dataType);
-		log.info("Request Data Info  :\n" + dataInfo);
+		log.info("Request Data Info  :\n" + paser(dataInfo));
 	}
 	
 	public static void requestInfo(String method, String dataType, Object dataInfo)
@@ -31,7 +33,7 @@ public class ServerLogger
 		log.info("--------- Http Server Invoker Start ---------");
 		log.info("Request Method     : " + method);
 		log.info("Request Data Type  : " + dataType);
-		log.info("Request Data Info  :\n" + dataInfo);
+		log.info("Request Data Info  :\n" + paser(dataInfo));
 	}
 	
 	public static void responseInfo(String charset, String dataType, Object dataInfo)
@@ -39,7 +41,7 @@ public class ServerLogger
 		log.info("---------------------------------------------");
 		log.info("Response Charset   : " + charset);
 		log.info("Response Data Type : " + dataType);
-		log.info("Response Data Info :\n" + dataInfo);
+		log.info("Response Data Info :\n" + paser(dataInfo));
 		log.info("---------- Http Server Invoker End ----------");
 	}
 	
@@ -47,7 +49,29 @@ public class ServerLogger
 	{
 		log.info("---------------------------------------------");
 		log.info("Response Data Type : " + dataType);
-		log.info("Response Data Info :\n" + dataInfo);
+		log.info("Response Data Info :\n" + paser(dataInfo));
 		log.info("---------- Http Server Invoker End ----------");
+	}
+	
+	public static String paser(Object dataInfo)
+	{
+		if (dataInfo instanceof List)
+		{
+			List list = (List) dataInfo;
+			int size = list.size();
+			
+			String classInfo = "List";
+			String containClassInfo = "";
+			if (size > 0)
+			{
+				containClassInfo = "[" + list.get(0).getClass().getName() + "]";
+			}	
+
+			String sizeInfo = ", Size is " + size;
+			
+			return  classInfo + containClassInfo + sizeInfo;
+		}
+		
+		return dataInfo.toString();
 	}
 }
--- java/nepxion-utility/src/com/nepxion/util/servlet/ServletLogger.java
+++ java/nepxion-utility/src/com/nepxion/util/servlet/ServletLogger.java
@@ -10,6 +10,8 @@ package com.nepxion.util.servlet;
  * @version 1.0
  */
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
@@ -30,7 +32,29 @@ public class ServletLogger
 		log.info("--------- Http Server Invoker Start ---------");
 		log.info("Request Method     : " + method);
 		log.info("Request Data Type  : " + dataType);
-		log.info("Request Data Info  :\n" + dataInfo);
+		log.info("Request Data Info  :\n" + paser(dataInfo));
 		log.info("---------- Http Server Invoker End ----------");
 	}
+	
+	public static String paser(Object dataInfo)
+	{
+		if (dataInfo instanceof List)
+		{
+			List list = (List) dataInfo;
+			int size = list.size();
+			
+			String classInfo = "List";
+			String containClassInfo = "";
+			if (size > 0)
+			{
+				containClassInfo = "[" + list.get(0).getClass().getName() + "]";
+			}	
+
+			String sizeInfo = ", Size is " + size;
+			
+			return  classInfo + containClassInfo + sizeInfo;
+		}
+		
+		return dataInfo.toString();
+	}	
 }
