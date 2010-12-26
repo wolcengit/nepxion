28581a46727c15c8bd7db095525bc1f3706cb312
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-12-26 02:39:23 +0000

    Initialize version

---
+++ java/nepxion-swing/src/com/nepxion/swing/toggle/JQueryTogglePanel.java
@@ -0,0 +1,32 @@
+package com.nepxion.swing.toggle;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import com.nepxion.swing.icon.IconFactory;
+import com.nepxion.swing.locale.SwingLocale;
+
+public abstract class JQueryTogglePanel
+	extends JThreadTogglePanel
+{
+	public JQueryTogglePanel(String queryTarget)
+	{
+		super(SwingLocale.getString("query_and_wait"));
+		
+		setToggleBannerIcon(IconFactory.getSwingIcon("banner/edit.png"));
+		setInformationText(SwingLocale.getString("query") + " " + SwingLocale.getString("quotation_left") + queryTarget + SwingLocale.getString("quotation_right") + " " + SwingLocale.getString("relative_data"));
+		setInformationIcon(IconFactory.getSwingIcon("banner/query_128.png"));
+		setErrorText(SwingLocale.getString("query") + " " + SwingLocale.getString("quotation_left") + queryTarget + SwingLocale.getString("quotation_right") + " " + SwingLocale.getString("relative_data_exception"));
+		setErrorIcon(IconFactory.getSwingIcon("banner/error_128.png"));
+		setWidth(300);
+		
+		showInformation();
+	}
+}
