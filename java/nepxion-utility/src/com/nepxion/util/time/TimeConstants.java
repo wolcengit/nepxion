300995699df214f1d6200ffc2da9f6b3f9c857ab
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2010-12-30 08:59:27 +0000

    Initialize version

---
+++ java/nepxion-utility/src/com/nepxion/util/time/TimeConstants.java
@@ -0,0 +1,21 @@
+package com.nepxion.util.time;
+
+/**
+ * <p>Title: Nepxion Utility</p>
+ * <p>Description: Nepxion Utility Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+public interface TimeConstants
+{
+	public final static String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	public final static String MILLISECOND_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss:SSS";
+	public final static String TIME_ZONE_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss zzzz";
+	public final static String DATE_FORMAT = "yyyy-MM-dd";
+	public final static String TIME_FORMAT = "HH:mm:ss";
+	public final static String MILLISECOND_TIME_FORMAT = "HH:mm:ss:SSS";
+}
