6d1ee6b454134965cabf72a3ebfc04fde338e5e7
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-03-16 12:17:44 +0000

    Initialize version

---
+++ java/nepxion-swing/src/com/nepxion/swing/popupmenu/PopupMenuManager.java
@@ -0,0 +1,41 @@
+package com.nepxion.swing.popupmenu;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.awt.Color;
+
+import javax.swing.JPopupMenu;
+
+import com.nepxion.swing.border.LineBorder;
+import com.nepxion.swing.lookandfeel.LookAndFeelManager;
+
+public class PopupMenuManager
+{
+	public static void setPreferenceStyle(JPopupMenu popupMenu)
+	{
+		if (LookAndFeelManager.isNimbusLookAndFeel())
+		{
+			popupMenu.setBorder(new LineBorder(new Color(89, 89, 89), 1));
+		}	
+	}
+	
+	public static int getMargin()
+	{
+		if (LookAndFeelManager.isNimbusLookAndFeel())
+		{
+			return 1;
+		}
+		else
+		{
+			return 2;
+		}
+	}
+}
---
+++ java/nepxion-swing/src/com/nepxion/swing/selector/calendar/CalendarManager.java
@@ -0,0 +1,81 @@
+package com.nepxion.swing.selector.calendar;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+
+import com.nepxion.swing.combobox.ComboBoxManager;
+import com.nepxion.swing.dimension.DimensionManager;
+import com.nepxion.swing.lookandfeel.LookAndFeelManager;
+
+public class CalendarManager
+{
+	public static void setPreferenceStyle(JComboBox comboBox)
+	{
+		if (LookAndFeelManager.isNimbusLookAndFeel())
+		{
+			JComboBox component = new JComboBox();
+			
+			comboBox.setBorder(component.getBorder());
+		}
+		else
+		{
+			ComboBoxManager.setPreferenceStyle(comboBox);
+		}
+	}
+	
+	public static void setPreferenceStyle(JButton button)
+	{
+		if (LookAndFeelManager.isNimbusLookAndFeel())
+		{
+			DimensionManager.setDimension(button, new Dimension(29, 29));
+		}
+	}
+	
+	public static void setPreferenceStyle(JSpinner spinner)
+	{
+		if (LookAndFeelManager.isNimbusLookAndFeel())
+		{
+			JTextField component = new JTextField();
+			
+			spinner.getEditor().setBorder(component.getBorder());
+		}
+	}
+	
+	public static int getCalendarSelectorDialogHeight()
+	{
+		if (LookAndFeelManager.isNimbusLookAndFeel())
+		{
+			return 310;
+		}
+		else
+		{	
+			return 280;
+		}
+	}
+	
+	public static int getDaySelectorDialogHeight()
+	{
+		if (LookAndFeelManager.isNimbusLookAndFeel())
+		{
+			return 280;
+		}
+		else
+		{	
+			return 250;
+		}
+	}	
+}
