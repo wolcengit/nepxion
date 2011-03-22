298848cc701e0ddaccc526c16a0d9896f338473b
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-03-22 17:46:34 +0000

    Refactory

--- java/nepxion-swing/src/com/nepxion/swing/scrollpane/JAutoRollScrollPane.java
+++ java/nepxion-swing/src/com/nepxion/swing/scrollpane/JAutoRollScrollPane.java
@@ -12,13 +12,30 @@ package com.nepxion.swing.scrollpane;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.plaf.UIResource;
+import javax.swing.plaf.basic.BasicButtonUI;
+
+import com.jidesoft.icons.JideIconsFactory;
 import com.jidesoft.swing.SimpleScrollPane;
-import com.nepxion.swing.border.BorderManager;
+import com.nepxion.swing.button.ButtonManager;
 
 public class JAutoRollScrollPane
-	extends SimpleScrollPane
-{
+	extends SimpleScrollPane implements SwingConstants
+{	
 	public JAutoRollScrollPane(Component view, int vsbPolicy, int hsbPolicy)
 	{
 		super(view, vsbPolicy, hsbPolicy);
@@ -48,14 +65,182 @@ public class JAutoRollScrollPane
 	}
 	
 	private void initComponents()
-	{
-		setViewportBorder(BorderManager.createLineBorder(Color.gray, 0, 0, 0, 0));
+	{					
+		setScrollUpButton(new JAutoRollScrollButton(SwingConstants.NORTH));
+		setScrollDownButton(new JAutoRollScrollButton(SwingConstants.SOUTH));
+		setScrollLeftButton(new JAutoRollScrollButton(SwingConstants.WEST));
+		setScrollRightButton(new JAutoRollScrollButton(SwingConstants.EAST));
+		
+		ButtonManager.updateUI(this, new Dimension(10, 10));
+		
+		setRepeatDelay(15);
+		
+		// setViewportBorder(BorderManager.createLineBorder(Color.gray, 0, 0, 0, 0));
 		
 		ScrollPaneManager.setPreferenceStyle(this);
 	}
 	
-	public void setLabelStyle()
+	public class JAutoRollScrollButton
+		extends JButton implements MouseListener, ActionListener, UIResource
 	{
-		ScrollPaneManager.setLabelStyle(this);
+		private int direction;
+		private Timer timer;
+		
+		public JAutoRollScrollButton(final int direction)
+		{
+			this.direction = direction;
+			
+			switch (direction)
+			{
+				case NORTH:
+					setIcon(JideIconsFactory.getImageIcon(JideIconsFactory.Arrow.UP));
+					break;
+				case SOUTH:
+					setIcon(JideIconsFactory.getImageIcon(JideIconsFactory.Arrow.DOWN));
+					break;
+				case WEST:
+					setIcon(JideIconsFactory.getImageIcon(JideIconsFactory.Arrow.LEFT));
+					break;
+				case EAST:
+					setIcon(JideIconsFactory.getImageIcon(JideIconsFactory.Arrow.RIGHT));
+					break;
+			}
+			
+			addActionListener(this);
+			addMouseListener(this);
+			
+			setUI(new BasicButtonUI()
+			{
+				public void paint(Graphics g, JComponent c)
+				{
+					super.paint(g, c);
+					
+					g.setColor(Color.gray);
+					
+					Border viewportBorder = getViewportBorder();
+					
+					if (viewportBorder != null && viewportBorder instanceof CompoundBorder)
+					{	
+						switch (direction)
+						{
+							case NORTH:
+								g.drawLine(0, 0, getWidth() - 1, 0);
+								g.drawLine(0, 0, 0, getHeight() - 1);
+								g.drawLine(getWidth() - 1, 0, getWidth() - 1, getHeight() - 1);
+								break;
+							case SOUTH:
+								g.drawLine(0, getHeight() - 1, getWidth() - 1, getHeight() - 1);
+								g.drawLine(0, 0, 0, getHeight() - 1);
+								g.drawLine(getWidth() - 1, 0, getWidth() - 1, getHeight() - 1);								
+								break;
+							case WEST:
+								g.drawLine(0, 0, getWidth() - 1, 0);
+								g.drawLine(0, 0, 0, getHeight() - 1);
+								g.drawLine(0, getHeight() - 1, getWidth() - 1, getHeight() - 1);
+								break;
+							case EAST:
+								g.drawLine(0, 0, getWidth() - 1, 0);
+								g.drawLine(getWidth() - 1, 0, getWidth() - 1, getHeight() - 1);
+								g.drawLine(0, getHeight() - 1, getWidth() - 1, getHeight() - 1);
+								break;
+						}
+					}
+					else
+					{
+						g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
+					}
+				}
+			}
+			);
+		}
+		
+		public void actionPerformed(ActionEvent e)
+		{
+			scroll(getViewport(), direction);
+			
+			updateButtonState();
+		}
+		
+		public void mouseClicked(MouseEvent e)
+		{
+		}
+		
+		public void mousePressed(MouseEvent e)
+		{
+			if (!isScrollOnRollover())
+			{
+				startTimer(e, 500);
+			}
+			else
+			{
+				updateTimer(e);
+			}
+		}
+		
+		public void mouseReleased(MouseEvent e)
+		{
+			if (!isScrollOnRollover())
+			{
+				stopTimer();
+			}
+			else
+			{
+				updateTimer(e);
+			}
+		}
+		
+		public void mouseEntered(MouseEvent e)
+		{
+			if (isScrollOnRollover())
+			{
+				startTimer(e, 500);
+			}
+		}
+		
+		private void updateTimer(MouseEvent e)
+		{
+			if (timer != null)
+			{
+				timer.setDelay(getDelay(e));
+			}
+		}
+		
+		private void startTimer(MouseEvent e, int initDelay)
+		{
+			stopTimer();
+			
+			timer = new Timer(getDelay(e), this);
+			timer.setInitialDelay(initDelay);
+			timer.start();
+		}
+		
+		private void stopTimer()
+		{
+			if (timer != null)
+			{
+				timer.stop();
+				timer = null;
+			}
+		}
+		
+		private int getDelay(MouseEvent e)
+		{
+			if (isScrollOnRollover())
+			{
+				return SwingUtilities.isLeftMouseButton(e) ? getRepeatDelay() : getRepeatDelay() * 2;
+			}
+			else
+			{
+				return getRepeatDelay();
+			}
+		}
+		
+		public void mouseExited(MouseEvent e)
+		{
+			if (isScrollOnRollover())
+			{
+				stopTimer();
+			}
+		}
 	}
 }
