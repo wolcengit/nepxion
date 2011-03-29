8054b2451d49e868b303ebf1a8832662c6309fdd
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-03-29 05:54:26 +0000

    Refactory

--- java/nepxion-swing-demo/release/applet/sign/sign-[util-jar].bat
+++ java/nepxion-swing-demo/release/applet/sign/sign-[util-jar].bat
@@ -23,8 +23,11 @@ set JAVA_HOME=D:\Software\JDK\JDK1.5
 %JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/util/http-client-4.0.3.jar security
 %JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/util/http-core-4.1.jar security
 %JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/util/jacob-1.15.jar security
+%JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/util/jdom-1.1.1.jar security
 %JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/util/json.jar security
+%JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/util/junit-4.8.2.jar security
 %JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/util/log4j-1.2.16.jar security
+%JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/util/ojdbc14-10g.jar security
 %JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/util/poi-3.7-20101029.jar security
 %JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/util/proxool-0.9.1.jar security
 %JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/util/proxool-cglib.jar security
