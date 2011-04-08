4581199445c4519177d507618850218d2c8dc728
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-04-08 15:38:31 +0000

    Refactory

--- java/nepxion-swing/src/com/nepxion/swing/searcher/zone/local/JZoneSelectorPanel.java
+++ java/nepxion-swing/src/com/nepxion/swing/searcher/zone/local/JZoneSelectorPanel.java
@@ -12,17 +12,20 @@ package com.nepxion.swing.searcher.zone.local;
 
 import java.awt.Dimension;
 
+import javax.swing.AbstractButton;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import com.nepxion.swing.button.ButtonManager;
+import com.nepxion.swing.editor.table.ITableCellEditorPane;
 import com.nepxion.swing.textfield.number.JNumberTextField;
 
 public class JZoneSelectorPanel
-	extends JPanel
+	extends JPanel implements ITableCellEditorPane
 {
-	private JNumberTextField zoneTextField;
-	private JZoneSelectorButton zoneSelectorButton;
+	private JNumberTextField textField;
+	private JZoneSelectorButton button;
 	
 	private int maximumLength = 12;
 	
@@ -43,57 +46,65 @@ public class JZoneSelectorPanel
 	
 	public JZoneSelectorPanel(String value, int maximumLength)
 	{
-		zoneTextField = new JNumberTextField(maximumLength, 0);
+		textField = new JNumberTextField(maximumLength, 0);
 		
-		zoneSelectorButton = new JZoneSelectorButton(zoneTextField);
+		button = new JZoneSelectorButton(textField);
 		
 		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-		add(zoneTextField);
-		add(zoneSelectorButton);
+		add(textField);
+		add(button);
 		
 		ButtonManager.updateUI(this, new Dimension(26, 22));
 		
-		setZone(value);
+		setValue(value);
 	}
 	
-	public JNumberTextField getZoneTextField()
+	public JTextField getTextField()
 	{
-		return zoneTextField;
+		return textField;
 	}
 	
-	public String getZone()
+	public AbstractButton getButton()
 	{
-		return zoneTextField.getText();
+		return button;
 	}
 	
-	public void setZone(String value)
+	public Object getValue()
 	{
+		return textField.getText();
+	}
+	
+	public void setValue(Object value)
+	{
+		String text = null;
+		
 		if (value == null)
 		{
-			value = "";
+			text = "";
 		}
 		
-		value = value.trim();
+		text = value.toString().trim();
 		
-		if (value.length() > maximumLength)
+		if (text.length() > maximumLength)
 		{
 			throw new IllegalArgumentException("Invalid value");
 		}
 		
-		if (!value.equals(""))
+		if (!text.equals(""))
 		{
-			boolean flag = value.matches("[0-9]+");
+			boolean flag = text.matches("[0-9]+");
 			if (!flag)
 			{
 				throw new IllegalArgumentException("Invalid value");
 			}
 		}
-		zoneTextField.setText(value);
+		
+		textField.setText(text);
 	}
 	
 	public void setEnabled(boolean enabled)
 	{
-		zoneTextField.setEditable(enabled);
-		zoneSelectorButton.setEnabled(enabled);
+		textField.setEditable(enabled);
+		button.setEnabled(enabled);
 	}
 }
