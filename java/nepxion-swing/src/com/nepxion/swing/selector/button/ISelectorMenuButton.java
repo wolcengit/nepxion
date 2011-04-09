546b7dfad7aa30e43f65f9ee788dd01c3d6cffb8
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-04-09 14:36:24 +0000

    Initialize version

---
+++ java/nepxion-swing/src/com/nepxion/swing/selector/button/ISelectorMenuButton.java
@@ -0,0 +1,30 @@
+package com.nepxion.swing.selector.button;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.awt.Component;
+
+import com.nepxion.swing.panel.option.JOptionButtonPanel;
+
+public interface ISelectorMenuButton
+{
+	public void setContentPane(Component component);
+	
+	public JOptionButtonPanel getOptionButtonPanel();
+	
+	public boolean doConfirm();
+	
+	public boolean doCancel();
+	
+	public boolean confirm();
+	
+	public boolean cancel();
+}
