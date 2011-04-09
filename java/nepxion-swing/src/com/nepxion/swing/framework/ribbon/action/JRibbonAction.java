76481ba91a849f2264cad3609adaf3d67a3e9045
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-04-09 14:38:46 +0000

    Refactory

--- java/nepxion-swing/src/com/nepxion/swing/framework/ribbon/action/JRibbonAction.java
+++ java/nepxion-swing/src/com/nepxion/swing/framework/ribbon/action/JRibbonAction.java
@@ -158,19 +158,19 @@ public class JRibbonAction
 			}
 			catch (NullPointerException ex)
 			{
-				ExceptionTracer.traceException(ribbonContainer, SwingLocale.getString("component_not_initialized") + " [" + ribbonComponentClass + "]", ex); // Neptune
+				ExceptionTracer.traceException(ribbonContainer, SwingLocale.getString("component_not_initialized") + " [" + ribbonComponentClass + "]", ex);
 				
 				return;
 			}
 			catch (ClassCastException ex)
 			{
-				ExceptionTracer.traceException(ribbonContainer, SwingLocale.getString("component_not_initialized") + " " + IRibbonComponent.class.getSimpleName() + " [" + ribbonComponentClass + "]", ex); // Neptune
+				ExceptionTracer.traceException(ribbonContainer, SwingLocale.getString("component_not_initialized") + " " + IRibbonComponent.class.getSimpleName() + " [" + ribbonComponentClass + "]", ex);
 				
 				return;
 			}
 			catch (Exception ex)
 			{
-				ExceptionTracer.traceException(ribbonContainer, SwingLocale.getString("component_instantiation_error") + " [" + ribbonComponentClass + "]", ex); // Neptune
+				ExceptionTracer.traceException(ribbonContainer, SwingLocale.getString("component_instantiation_error") + " [" + ribbonComponentClass + "]", ex);
 				
 				return;
 			}
