fdca09f529aa362efe97460cc682540978d47b69
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-04-10 14:00:49 +0000

    Initialize version

---
+++ java/nepxion-swing/src/com/nepxion/swing/renderer/table/TableDateTimeCellRenderer.java
@@ -0,0 +1,44 @@
+package com.nepxion.swing.renderer.table;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.awt.Component;
+import java.util.Date;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+import com.nepxion.util.time.TimeUtil;
+
+public class TableDateTimeCellRenderer
+	extends DefaultTableCellRenderer
+{
+	public TableDateTimeCellRenderer(int cellAlignment)
+	{
+		setHorizontalAlignment(cellAlignment);
+		setOpaque(true);
+	}
+	
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
+	{
+		super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		
+		if (value instanceof Date)
+		{
+			Date date = (Date) value;
+			String text = TimeUtil.getDateTime(date);
+			
+			setText(text);
+		}
+		
+		return this;
+	}
+}
