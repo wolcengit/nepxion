9b6191495baf5665c91a99ab4d8e199f2a5c2d68
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-04-12 06:03:12 +0000

    Refactory

--- java/nepxion-swing/src/com/nepxion/swing/list/JBasicList.java
+++ java/nepxion-swing/src/com/nepxion/swing/list/JBasicList.java
@@ -10,15 +10,21 @@ package com.nepxion.swing.list;
  * @version 1.0
  */
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.List;
 import java.util.Vector;
 
 import javax.swing.JList;
 import javax.swing.ListModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class JBasicList
-	extends JList
+	extends JList implements ListSelectionListener, MouseListener
 {
+	private int selectedRow = -1;
+	
 	public JBasicList()
 	{
 		super();
@@ -50,6 +56,8 @@ public class JBasicList
 	private void initComponents()
 	{
 		ListManager.setPreferenceStyle(this);
+		
+		addListSelectionListener(this);
 	}
 	
 	public List getListData()
@@ -64,5 +72,101 @@ public class JBasicList
 		}
 		
 		return listData;
+	}
+	
+	public void valueChanged(ListSelectionEvent e)
+	{		
+		boolean isAdjusting = e.getValueIsAdjusting();
+		if (isAdjusting)
+		{
+			if (getSelectedIndex() == selectedRow)
+			{
+				return;
+			}
+			
+			selectedRow = getSelectedIndex();
+			
+			executeSelection(selectedRow);
+		}
+	}
+	
+	public void executeSelection(int selectedRow)
+	{
+		
+	}
+	
+	public void mouseClicked(MouseEvent e)
+	{
+		int selectedRow = getSelectedIndex();
+		
+		executeClicked(selectedRow, e);
+	}
+	
+	public void executeClicked(int selectedRow, MouseEvent e)
+	{
+		if (e.getClickCount() > 1)
+		{
+			executeDoubleClicked(selectedRow);
+		}
+		else
+		{
+			executeClicked(selectedRow);
+		}
+	}
+	
+	public void executeClicked(int selectedRow)
+	{
+		
+	}
+	
+	public void executeDoubleClicked(int selectedRow)
+	{
+		
+	}
+	
+	public void mouseEntered(MouseEvent e)
+	{
+		
+	}
+	
+	public void mouseExited(MouseEvent e)
+	{
+		
+	}
+	
+	public void mousePressed(MouseEvent e)
+	{
+		
+	}
+	
+	public void mouseReleased(MouseEvent e)
+	{
+		if (!e.isPopupTrigger())
+		{
+			return;
+		}
+		
+		int x = e.getX();
+		int y = e.getY();
+		
+		int selectedRow = getSelectedIndex();
+		if (selectedRow > -1)
+		{
+			executePopupMenu(selectedRow, x, y);
+		}
+		else
+		{
+			executePopupMenu(x, y);
+		}
+	}
+	
+	public void executePopupMenu(int selectedRow, int x, int y)
+	{
+		
+	}
+	
+	public void executePopupMenu(int x, int y)
+	{
+		
 	}
 }
