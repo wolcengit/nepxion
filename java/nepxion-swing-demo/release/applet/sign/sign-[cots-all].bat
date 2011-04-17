2bdeb32fe508da42320147d6b0325106581999d8
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-04-17 08:52:17 +0000

    Initialize version

---
+++ java/nepxion-swing-demo/release/applet/sign/sign-[cots-all].bat
@@ -0,0 +1,21 @@
+@echo on
+@echo =============================================================
+@echo $                                                           $
+@echo $                     Nepxion Signature                     $
+@echo $                                                           $
+@echo $                                                           $
+@echo $                                                           $
+@echo $  Nepxion Technologies All Right Reserved                  $
+@echo $  Copyright(C) 2010                                        $
+@echo $                                                           $
+@echo =============================================================
+@echo.
+@echo off
+
+@title Nepxion Signature
+@color 0a
+
+set JAVA_HOME=D:\Software\JDK\JDK1.5
+
+%JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/cots/twaver.jar security
+%JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/cots/layouter.jar security
