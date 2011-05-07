f8ca7caa9a89ea030497712a1c567c7567b63e29
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-05-07 09:33:26 +0000

    Initialize version

---
+++ java/nepxion-cots/src/com/nepxion/cots/twaver/gis/TGisNavigator.java
@@ -0,0 +1,31 @@
+package com.nepxion.cots.twaver.gis;
+
+/**
+ * <p>Title: Nepxion Cots For TWaver</p>
+ * <p>Description: Nepxion Cots Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * <p>Announcement: It depends on the commercial software of TWaver, the repository only references the trial version.</p>
+ * <p>If you want to use Nepxion Cots, please contact with Serva Corporation to buy the genuine version.</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import twaver.gis.gadget.Navigator;
+
+public class TGisNavigator
+	extends Navigator
+{
+	public TGisNavigator(TGisGraph gisGraph)
+	{
+		super(gisGraph);
+	}
+	
+	public void showout(boolean showout)
+	{		
+		super.showout(showout);
+		
+		setPosition(500, 0);
+	}
+}
