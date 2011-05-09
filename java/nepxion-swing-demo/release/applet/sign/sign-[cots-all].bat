5741d369f655986ff82651f807b53c12cbd3fc2f
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-05-09 04:58:40 +0000

    Refactory

--- java/nepxion-swing-demo/release/applet/sign/sign-[cots-all].bat
+++ java/nepxion-swing-demo/release/applet/sign/sign-[cots-all].bat
@@ -18,4 +18,5 @@
 set JAVA_HOME=D:\Software\JDK\JDK1.5
 
 %JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/cots/twaver.jar security
+%JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/cots/twavergis.jar security
 %JAVA_HOME%\bin\jarsigner -keystore security.store -storepass 888888 ../lib/cots/layouter.jar security
