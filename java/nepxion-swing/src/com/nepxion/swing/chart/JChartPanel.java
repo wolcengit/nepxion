6630e5bea6eb2d56aaa8df405665136c3af5630c
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-05-20 03:04:38 +0000

    Refactory

--- java/nepxion-swing/src/com/nepxion/swing/chart/JChartPanel.java
+++ java/nepxion-swing/src/com/nepxion/swing/chart/JChartPanel.java
@@ -130,7 +130,7 @@ public class JChartPanel
 	}
 	
     /**
-     * Invoked when a mouse button has been released on a component.
+     * Invoked when the mouse button has been released on a component.
      * @param e the instance of MouseEvent
      */
 	public void mouseReleased(MouseEvent e)
