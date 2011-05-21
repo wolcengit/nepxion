f1a1beccfdfa361910e3a4487978353080f3d7fe
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-05-21 11:49:16 +0000

    Refactory

--- java/nepxion-swing/src/com/nepxion/swing/dimension/DimensionManager.java
+++ java/nepxion-swing/src/com/nepxion/swing/dimension/DimensionManager.java
@@ -15,16 +15,25 @@ import java.awt.Dimension;
 
 public class DimensionManager
 {
+	/**
+	 * Adapts the component dimension with the preferred size.
+	 * @param component the instance of Component
+	 */
 	public static void adaptDimension(Component component)
 	{
 		setDimension(component, component.getPreferredSize());
 	}
 	
+	/**
+	 * Sets the component dimension by a dimension.
+	 * @param component the instance of Component
+	 * @param dimension the instance of Dimenion
+	 */
 	public static void setDimension(Component component, Dimension dimension)
 	{
 		component.setPreferredSize(dimension);
 		component.setMaximumSize(dimension);
 		component.setMinimumSize(dimension);
-		component.setSize(dimension);		
+		component.setSize(dimension);
 	}
 }
