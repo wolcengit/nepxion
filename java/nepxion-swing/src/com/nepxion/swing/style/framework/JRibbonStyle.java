3f0557f9228438739d35285630fb2f07eae7a876
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-05-28 18:12:38 +0000

    Initialize version

---
+++ java/nepxion-swing/src/com/nepxion/swing/style/framework/JRibbonStyle.java
@@ -0,0 +1,47 @@
+package com.nepxion.swing.style.framework;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.UIManager;
+
+import com.nepxion.swing.border.RaisedHeaderBorder;
+import com.nepxion.swing.border.ShadowBorder;
+
+public class JRibbonStyle
+	extends AbstractStyle
+{
+	public static final String ID = JRibbonStyle.class.getName();
+	
+	public JRibbonStyle()
+	{
+		gradientColor = new Color(115, 164, 209);
+		selectionGradientColor = new Color(57, 105, 138);
+		
+		foreground = Color.white;
+		selectionForeground = Color.white;
+		
+		background = UIManager.getColor("Panel.background");
+		selectionBackground = UIManager.getColor("Panel.background");
+		
+		tabbedPaneGradientColor = new Color(236, 244, 254);
+		tabbedPaneBackground = UIManager.getColor("Panel.background");
+		
+		tabbedPaneForeground = Color.black;
+		tabbedPaneSelectionForeground = Color.black;
+		
+		border = new ShadowBorder(new Color(167, 172, 180));
+		headerBorder = new RaisedHeaderBorder(Color.white, new Color(182, 186, 194)); // new Color(167, 172, 180)
+		labelBorder = BorderFactory.createEmptyBorder(2, 4, 2, 1);
+	}
+}
