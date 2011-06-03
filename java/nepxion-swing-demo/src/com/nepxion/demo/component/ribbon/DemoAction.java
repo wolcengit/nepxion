cbb255975db54996ce3ecf27d6bfe40217e45c66
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-06-03 17:21:23 +0000

    Initialize version

---
+++ java/nepxion-swing-demo/src/com/nepxion/demo/component/ribbon/DemoAction.java
@@ -0,0 +1,29 @@
+package com.nepxion.demo.component.ribbon;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+
+import com.nepxion.swing.action.JSecurityAction;
+import com.nepxion.swing.handle.HandleManager;
+import com.nepxion.swing.optionpane.JBasicOptionPane;
+
+public class DemoAction
+	extends JSecurityAction
+{
+	public void execute(ActionEvent e)
+	{
+		Component component = (Component) e.getSource();
+
+		JBasicOptionPane.showMessageDialog(HandleManager.getFrame(component), getText());		
+	}
+}
