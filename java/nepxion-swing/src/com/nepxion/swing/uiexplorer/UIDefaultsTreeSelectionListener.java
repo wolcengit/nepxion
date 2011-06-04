f16eb304745ed512f98a05d20abe4f3d265c5a5c
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-06-04 18:42:59 +0000

    Initialize version

---
+++ java/nepxion-swing/src/com/nepxion/swing/uiexplorer/UIDefaultsTreeSelectionListener.java
@@ -0,0 +1,66 @@
+package com.nepxion.swing.uiexplorer;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.util.Map;
+
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.table.TableModel;
+import javax.swing.tree.TreePath;
+
+import com.nepxion.swing.framework.dockable.JDockableView;
+import com.nepxion.swing.handle.HandleManager;
+import com.nepxion.swing.scrollpane.JBasicScrollPane;
+import com.nepxion.swing.table.layoutable.TableCellRendererLayout;
+
+public class UIDefaultsTreeSelectionListener
+	implements TreeSelectionListener
+{
+	private UIDefaultsTable table;
+	
+	public void valueChanged(TreeSelectionEvent evt)
+	{
+		UIDefaultsTree tree = (UIDefaultsTree) evt.getSource();
+		TreePath treePath = tree.getSelectionPath();
+		if (treePath != null)
+		{
+			if (table == null)
+			{
+				UIDefaultsHierarchy hierarchy = (UIDefaultsHierarchy) HandleManager.getComponent(tree, UIDefaultsHierarchy.class);
+				JDockableView dockableView = hierarchy.getDockableView(UIDefaultsHierarchy.CONTENT_PANE_TITLE);
+				JBasicScrollPane scrollPane = (JBasicScrollPane) dockableView.getContentPane();
+				table = (UIDefaultsTable) scrollPane.getViewport().getView();
+			}
+			
+			UIDefaultsTreeNode treeNode = (UIDefaultsTreeNode) treePath.getLastPathComponent();
+			Object key = treeNode.getKey();
+			Object value = treeNode.getValue();
+			
+			if (value != null)
+			{
+				Map map = (Map) value;
+				
+				TableModel tableModel = table.getModel();
+				if (tableModel != null && tableModel instanceof UIDefaultsTableModel)
+				{
+					((UIDefaultsTableModel) tableModel).setRowData(map);
+				}
+				else
+				{
+					table.setModel(new UIDefaultsTableModel(map));
+				}
+				TableCellRendererLayout layout = new TableCellRendererLayout(table);
+				layout.doLayout(new int[][] { {300, 400, 100}, {}});
+			}
+		}
+	}
+}
