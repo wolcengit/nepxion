64733e7e3cf43210583ea9d65279b363c7e25cdf
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-06-16 07:08:09 +0000

    Refactory

---
+++ java/nepxion-swing/src/com/nepxion/swing/selector/color/IColorSelectorComponent.java
@@ -0,0 +1,28 @@
+package com.nepxion.swing.selector.color;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.awt.Color;
+
+public interface IColorSelectorComponent
+{
+	/**
+	 * Gets the color selector.
+	 * @return the instance of JColorSelector
+	 */
+	public JColorSelector getColorSelector();
+	
+	/**
+	 * Executes the color selection.
+	 * @param color the instance of Color
+	 */
+	public void executeSelection(Color color);
+}
