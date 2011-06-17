2914c593515523c63ecfc706b851c1d3a5d25f21
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-06-17 12:24:49 +0000

    Refactory

--- java/nepxion-swing/src/com/nepxion/swing/menu/JBasicMenu.java
+++ java/nepxion-swing/src/com/nepxion/swing/menu/JBasicMenu.java
@@ -17,11 +17,36 @@ import javax.swing.JMenu;
 public class JBasicMenu
 	extends JMenu
 {
+	/**
+	 * Constructs with the default.
+	 */
 	public JBasicMenu()
 	{
 		super();
 	}
 	
+	/**
+	 * Constructs with the specified initial action.
+	 * @param action the instance of Action
+	 */
+	public JBasicMenu(Action action)
+	{
+		super(action);
+	}
+	
+	/**
+	 * Constructs with the specified initial text.
+	 * @param text the text string
+	 */
+	public JBasicMenu(String text)
+	{
+		super(text);
+	}
+	
+	/**
+	 * Constructs with the specified initial icon.
+	 * @param icon the instance of Icon
+	 */
 	public JBasicMenu(Icon icon)
 	{
 		super();
@@ -29,6 +54,47 @@ public class JBasicMenu
 		setIcon(icon);
 	}
 	
+	/**
+	 * Constructs with the specified initial text and icon.
+	 * @param text the text string 
+	 * @param icon the icon of Icon
+	 */
+	public JBasicMenu(String text, Icon icon)
+	{
+		super(text);
+		
+		setIcon(icon);
+	}
+	
+	/**
+	 * Constructs with the specified initial text and tooltip text.
+	 * @param text the text string
+	 * @param toolTipText the tooltip text string
+	 */
+	public JBasicMenu(String text, String toolTipText)
+	{
+		super(text);
+		
+		setToolTipText(toolTipText);
+	}
+	
+	/**
+	 * Constructs with the specified initial text and selected.
+	 * @param text the text string
+	 * @param selected the boolean value of selected
+	 */
+	public JBasicMenu(String text, boolean selected)
+	{
+		super(text);
+		
+		setSelected(selected);
+	}
+	
+	/**
+	 * Constructs with the specified initial icon and tooltip text.
+	 * @param icon the instance of Icon
+	 * @param toolTipText the tooltip text string
+	 */
 	public JBasicMenu(Icon icon, String toolTipText)
 	{
 		super();
@@ -37,30 +103,25 @@ public class JBasicMenu
 		setToolTipText(toolTipText);
 	}
 	
-	public JBasicMenu(String text)
+	/**
+	 * Constructs with the specified initial icon and selected.
+	 * @param icon the instance of Icon
+	 * @param selected the boolean value of selected
+	 */
+	public JBasicMenu(Icon icon, boolean selected)
 	{
-		super(text);
-	}
-	
-	public JBasicMenu(String text, String toolTipText)
-	{
-		super(text);
-		
-		setToolTipText(toolTipText);
-	}
-	
-	public JBasicMenu(Action action)
-	{
-		super(action);
-	}
-	
-	public JBasicMenu(String text, Icon icon)
-	{
-		super(text);
+		super();
 		
 		setIcon(icon);
+		setSelected(selected);
 	}
 	
+	/**
+	 * Constructs with the specified initial text, icon and tooltip text.
+	 * @param text the text string
+	 * @param icon the instance of Icon
+	 * @param toolTipText the tooltip text string
+	 */
 	public JBasicMenu(String text, Icon icon, String toolTipText)
 	{
 		super(text);
@@ -69,18 +130,62 @@ public class JBasicMenu
 		setToolTipText(toolTipText);
 	}
 	
-	public JBasicMenu(String text, boolean selected)
+	/**
+	 * Constructs with the specified initial text, icon and selected.
+	 * @param text the text string
+	 * @param icon the instance of Icon
+	 * @param selected the boolean value of selected
+	 */
+	public JBasicMenu(String text, Icon icon, boolean selected)
 	{
 		super(text);
 		
+		setIcon(icon);
 		setSelected(selected);
 	}
 	
+	/**
+	 * Constructs with the specified initial text, tooltip text and selected.
+	 * @param text the text string
+	 * @param toolTipText the tooltip text string
+	 * @param selected the boolean value of selected
+	 */
 	public JBasicMenu(String text, String toolTipText, boolean selected)
 	{
 		super(text);
 		
-		setSelected(selected);
 		setToolTipText(toolTipText);
+		setSelected(selected);
+	}
+	
+	/**
+	 * Constructs with the specified initial icon, tooltip text and selected.
+	 * @param icon the instance of Icon
+	 * @param toolTipText the tooltip text string
+	 * @param selected the boolean value of selected
+	 */
+	public JBasicMenu(Icon icon, String toolTipText, boolean selected)
+	{
+		super();
+		
+		setIcon(icon);
+		setToolTipText(toolTipText);
+		setSelected(selected);
+	}
+	
+	/**
+	 * Constructs with the specified initial text, icon, tooltip text and selected.
+	 * @param text the text string
+	 * @param icon the instance of Icon
+	 * @param toolTipText the tooltip text string
+	 * @param selected the boolean value of selected
+	 */
+	public JBasicMenu(String text, Icon icon, String toolTipText, boolean selected)
+	{
+		super(text);
+		
+		setIcon(icon);
+		setToolTipText(toolTipText);
+		setSelected(selected);
 	}
 }
