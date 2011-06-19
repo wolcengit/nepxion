6fe32a4da161085c88ee6f0fe64339a57a4c9f75
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-06-19 09:11:18 +0000

    Refactory

--- java/nepxion-swing/src/com/nepxion/swing/shrinkbar/JShrinkBar.java
+++ java/nepxion-swing/src/com/nepxion/swing/shrinkbar/JShrinkBar.java
@@ -11,34 +11,12 @@ package com.nepxion.swing.shrinkbar;
  */
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Component;
-import java.awt.Font;
-
-import javax.swing.Icon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-import com.nepxion.swing.toolbar.JBasicToolBar;
 
 public class JShrinkBar
-	extends JPanel implements ShrinkContants
-{
-	private JShrinkHeader shrinkHeader;
-	
-	private JSplitBar splitBar;
-	
+	extends BasicShrinkBar
+{	
 	private Component contentPane;
-	
-	public JShrinkBar()
-	{
-		this(null);
-	}
-	
-	public JShrinkBar(Component contentPane)
-	{
-		this(PLACEMENT_WEST, contentPane);
-	}
 	
 	public JShrinkBar(int placement)
 	{
@@ -57,43 +35,13 @@ public class JShrinkBar
 	
 	public JShrinkBar(int placement, int contentPaneType, Component contentPane)
 	{
+		super(placement, contentPaneType);
+		
 		this.contentPane = contentPane;
-		
-		shrinkHeader = new JShrinkHeader(placement, contentPaneType)
-		{
-			public void setShrinked(boolean shrinked)
-			{
-				super.setShrinked(shrinked);
-				
-				splitBar.setShrinked(shrinked);
-			}
-		};		
-		splitBar = new JSplitBar(shrinkHeader, 3);
-		
-		setLayout(new BorderLayout());
-		add(shrinkHeader, BorderLayout.NORTH);
-		
-		togglePlacement();
 		
 		if (contentPane != null)
 		{
 			setContentPane(contentPane);
-		}
-	}
-	
-	protected void togglePlacement()
-	{
-		int placement = shrinkHeader.getPlacement();
-		
-		splitBar.setPlacement(placement);
-		
-		if (placement == PLACEMENT_EAST)
-		{
-			add(splitBar, BorderLayout.WEST);
-		}
-		else if (placement == PLACEMENT_WEST)
-		{
-			add(splitBar, BorderLayout.EAST);
 		}
 	}
 	
@@ -107,142 +55,5 @@ public class JShrinkBar
 		this.contentPane = contentPane;
 		
 		add(contentPane, BorderLayout.CENTER);
-	}
-	
-	public JShrinkHeader getShrinkHeader()
-	{
-		return shrinkHeader;
-	}
-	
-	public JLabel getShrinkLabel()
-	{
-		return shrinkHeader.getLabel();
-	}
-	
-	public JBasicToolBar getShrinkToolBar()
-	{
-		return shrinkHeader.getToolBar();
-	}
-	
-	public String getTitle()
-	{
-		if (shrinkHeader.getLabel() == null)
-		{
-			throw new IllegalArgumentException("It should be set content pane type with CONTENT_PANE_TYPE_LABEL");
-		}
-		
-		return shrinkHeader.getLabel().getText();
-	}
-	
-	public void setTitle(String title)
-	{
-		if (shrinkHeader.getLabel() == null)
-		{
-			throw new IllegalArgumentException("It should be set content pane type with CONTENT_PANE_TYPE_LABEL");
-		}
-		
-		shrinkHeader.getLabel().setText(title);
-	}
-	
-	public Color getTitleColor()
-	{
-		if (shrinkHeader.getLabel() == null)
-		{
-			throw new IllegalArgumentException("It should be set content pane type with CONTENT_PANE_TYPE_LABEL");
-		}
-		
-		return shrinkHeader.getLabel().getForeground();
-	}
-	
-	public void setTitleColor(Color color)
-	{
-		if (shrinkHeader.getLabel() == null)
-		{
-			throw new IllegalArgumentException("It should be set content pane type with CONTENT_PANE_TYPE_LABEL");
-		}
-		
-		shrinkHeader.getLabel().setForeground(color);
-	}
-	
-	public Font getTitleFont()
-	{
-		if (shrinkHeader.getLabel() == null)
-		{
-			throw new IllegalArgumentException("It should be set content pane type with CONTENT_PANE_TYPE_LABEL");
-		}
-		
-		return shrinkHeader.getLabel().getFont();
-	}
-	
-	public void setTitleFont(Font font)
-	{
-		if (shrinkHeader.getLabel() == null)
-		{
-			throw new IllegalArgumentException("It should be set content pane type with CONTENT_PANE_TYPE_LABEL");
-		}
-		
-		shrinkHeader.getLabel().setFont(font);
-	}
-	
-	public Icon getIcon()
-	{
-		if (shrinkHeader.getLabel() == null)
-		{
-			throw new IllegalArgumentException("It should be set content pane type with CONTENT_PANE_TYPE_LABEL");
-		}
-		
-		return shrinkHeader.getLabel().getIcon();
-	}
-	
-	public void setIcon(Icon icon)
-	{
-		if (shrinkHeader.getLabel() == null)
-		{
-			throw new IllegalArgumentException("It should be set content pane type with CONTENT_PANE_TYPE_LABEL");
-		}
-		
-		shrinkHeader.getLabel().setIcon(icon);
-	}
-	
-	public int getPlacement()
-	{
-		return shrinkHeader.getPlacement();
-	}
-	
-	public void setPlacement(int placement)
-	{
-		shrinkHeader.setPlacement(placement);
-		
-		togglePlacement();
-	}
-	
-	public int getContentPaneType()
-	{
-		return shrinkHeader.getContentPaneType();
-	}
-	
-	public void setContentPaneType(int contentPaneType)
-	{
-		shrinkHeader.setContentPaneType(contentPaneType);
-	}
-	
-	public boolean isShrinked()
-	{
-		return shrinkHeader.isShrinked();
-	}
-	
-	public void setShrinked(boolean shrinked)
-	{
-		shrinkHeader.setShrinked(shrinked);
-	}
-	
-	public int getShrinkedWidth()
-	{
-		return shrinkHeader.getShrinkedWidth();
-	}
-	
-	public void setShrinkedWidth(int shrinkedWidth)
-	{
-		shrinkHeader.setShrinkedWidth(shrinkedWidth);
 	}
 }
