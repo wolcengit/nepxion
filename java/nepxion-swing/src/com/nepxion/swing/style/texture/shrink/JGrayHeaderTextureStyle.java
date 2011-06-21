c428066b9c7e6a2b3a2e830fb12bdd746bee7a5f
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-06-21 04:47:01 +0000

    Refactory

--- java/nepxion-swing/src/com/nepxion/swing/style/texture/shrink/JGrayHeaderTextureStyle.java
+++ java/nepxion-swing/src/com/nepxion/swing/style/texture/shrink/JGrayHeaderTextureStyle.java
@@ -17,15 +17,21 @@ import com.nepxion.swing.style.texture.basic.JGrayTextureStyle;
 public class JGrayHeaderTextureStyle
 	extends JBasicHeaderTextureStyle
 {
+	/**
+	 * The style path.
+	 */
 	public static final String STYLE_PATH = "skin/shrink/gray/";
 	
+	/**
+	 * Constructs with the default.
+	 */
 	public JGrayHeaderTextureStyle()
 	{
 		super(JGrayTextureStyle.STYLE_PATH, STYLE_PATH);
 		
-		foreground = new Color(215, 215, 216);
-		selectionForeground = new Color(215, 215, 216);
+		foreground = new Color(59, 59, 59);
+		selectionForeground = new Color(59, 59, 59);
 		
-		borderColor = new Color(146, 151, 161);
+		borderColor = new Color(161, 169, 179);
 	}
 }
