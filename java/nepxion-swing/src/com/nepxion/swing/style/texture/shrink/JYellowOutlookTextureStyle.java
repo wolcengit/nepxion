f9977cbd5e1d5be4fa999d17f9bf0e3922f9547a
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-06-21 07:15:21 +0000

    Initialize version

---
+++ java/nepxion-swing/src/com/nepxion/swing/style/texture/shrink/JYellowOutlookTextureStyle.java
@@ -0,0 +1,38 @@
+package com.nepxion.swing.style.texture.shrink;
+
+/**
+ * <p>Title: Nepxion Swing</p>
+ * <p>Description: Nepxion Swing Repository</p>
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email hj_ren@msn.com
+ * @version 1.0
+ */
+
+import java.awt.Color;
+
+import com.nepxion.swing.style.texture.basic.JYellowTextureStyle;
+
+public class JYellowOutlookTextureStyle
+	extends JBasicOutlookTextureStyle
+{
+	/**
+	 * The style path.
+	 */
+	public static final String STYLE_PATH = "skin/outlook/yellow/";
+	
+	/**
+	 * Constructs with the default.
+	 */
+	public JYellowOutlookTextureStyle()
+	{
+		super(JYellowTextureStyle.STYLE_PATH, STYLE_PATH);
+		
+		foreground = new Color(120, 120, 125);
+		selectionForeground = new Color(109, 85, 2);
+		
+		borderColor = new Color(146, 151, 161);
+		scrollBorderColor = new Color(233, 223, 207);
+	}
+}
