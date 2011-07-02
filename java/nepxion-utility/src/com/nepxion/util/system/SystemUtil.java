ccd60435ea8322bea0c86f6000dc166d9bdc89d4
HaoJun.Ren <HaoJun.Ren@4900acfa-993c-71f3-3719-b31e1bbe1dc8>
2011-07-02 08:43:21 +0000

    Refactory

--- java/nepxion-utility/src/com/nepxion/util/system/SystemUtil.java
+++ java/nepxion-utility/src/com/nepxion/util/system/SystemUtil.java
@@ -14,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import com.nepxion.util.encoder.EncoderContext;
@@ -102,6 +103,30 @@ public class SystemUtil
 	}
 	
 	/**
+	 * Gets the environment variable by a key.
+	 * @param key the key string
+	 * @return the value string
+	 * @throws Exception
+	 */
+	public static String getEnvironmentVariable(String key)
+		throws Exception
+	{
+		Map environmentVariables = SystemUtil.getEnvironmentVariables();
+		
+		for (Iterator iterator = environmentVariables.keySet().iterator(); iterator.hasNext();)
+		{
+			String k = (String) iterator.next();
+			
+			if (k.trim().toLowerCase().equals(key.trim().toLowerCase()))
+			{
+				return (String) environmentVariables.get(k);
+			}
+		}	
+		
+		return null;
+	}
+	
+	/**
 	 * Gets the environment variables.
 	 * @return the instance of Map
 	 * @throws Exception
@@ -123,7 +148,79 @@ public class SystemUtil
 			while ((line = bufferedReader.readLine()) != null)
 			{
 				String[] stringArray = line.split("=");
-				map.put(stringArray[0], stringArray[1]);
+				map.put(stringArray[0].trim(), stringArray[1].trim());
+			}
+		}
+		catch (Exception e)
+		{
+			throw e;
+		}
+		finally
+		{			
+			if (bufferedReader != null)
+			{	
+				bufferedReader.close();
+			}
+		}
+		
+		return map;
+	}
+	
+	/**
+	 * Gets the reg environment variable by a key.
+	 * @param key the key string
+	 * @return the value string
+	 * @throws Exception
+	 */
+	public static String getRegEnvironmentVariable(String key)
+		throws Exception
+	{
+		Map environmentVariables = SystemUtil.getRegEnvironmentVariables();
+		
+		for (Iterator iterator = environmentVariables.keySet().iterator(); iterator.hasNext();)
+		{
+			String k = (String) iterator.next();
+			
+			if (k.trim().toLowerCase().equals(key.trim().toLowerCase()))
+			{
+				return (String) environmentVariables.get(k);
+			}
+		}	
+		
+		return null;
+	}
+	
+	/**
+	 * Gets the reg environment variables.
+	 * @return the instance of Map
+	 * @throws Exception
+	 */
+	public static Map getRegEnvironmentVariables()
+		throws Exception
+	{
+		Map map = new HashMap();
+		
+		Process process = Runtime.getRuntime().exec("cmd /c reg query \"HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment\\\" /v");
+		
+		InputStream inputStream = process.getInputStream();
+		InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+		
+		try
+		{
+			String line = null;
+			while ((line = bufferedReader.readLine()) != null)
+			{
+				if (line.indexOf("REG_SZ") > -1)
+				{
+					String[] stringArray = line.split("REG_SZ");
+					map.put(stringArray[0].trim(), stringArray[1].trim());
+				}
+				else if (line.indexOf("REG_EXPAND_SZ") > -1)
+				{
+					String[] stringArray = line.split("REG_EXPAND_SZ");
+					map.put(stringArray[0].trim(), stringArray[1].trim());
+				}
 			}
 		}
 		catch (Exception e)
